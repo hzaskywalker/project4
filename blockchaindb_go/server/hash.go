746401a0199e3216e8fc6136cdfc7aab166f1a4c
hqz true <hqz true>
2017-06-18 02:59:12 -0400

    ..

---
+++ blockchaindb_go/server/hash.go
@@ -0,0 +1,22 @@
+package main
+
+import "crypto/sha256"
+import "fmt"
+
+func GetHashString(String string) string {
+	return fmt.Sprintf("%x", GetHashBytes(String))
+}
+
+func GetHashBytes(String string) [32]byte {
+	return sha256.Sum256([]byte(String))
+}
+
+func CheckHash(Hash string) bool {
+	for i:=0; i<HashHardness; i++{
+        if Hash[i]!='0'{
+            return false
+        }
+    }
+	//Hash[0:5]=="00000"
+	return len(Hash) == 64
+}
